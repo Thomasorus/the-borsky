cbf1db74c36a1c3e14eab319e453399da65d4e63
Thomasorus <contact@thomasorus.com>
2021-06-30 11:31:21 +0200

    Updated default layout

--- www/site/templates/default.php
+++ www/site/templates/default.php
@@ -1,15 +1,17 @@
 <?php snippet('header') ?>
 <main>
-    <?php foreach ($page->text()->toLayouts() as $layout): ?>
-    <section class="grid" id="<?= $layout->id() ?>">
-    <?php foreach ($layout->columns() as $column): ?>
-    <div class="column" style="--span:<?= $column->span() ?>">
-        <div class="blocks">
-        <?= $column->blocks() ?>
+    <?php if($page->text()->isNotEmpty()): ?>
+        <?php foreach ($page->text()->toLayouts() as $layout): ?>
+        <section class="grid" id="<?= $layout->id() ?>">
+        <?php foreach ($layout->columns() as $column): ?>
+        <div class="column" style="--span:<?= $column->span() ?>">
+            <div class="blocks">
+            <?= $column->blocks() ?>
+            </div>
         </div>
-    </div>
-    <?php endforeach ?>
-    </section>
-    <?php endforeach ?>
+        <?php endforeach ?>
+        </section>
+        <?php endforeach ?>
+    <?php endif; ?>
 </main>
 <?php snippet('footer') ?>
