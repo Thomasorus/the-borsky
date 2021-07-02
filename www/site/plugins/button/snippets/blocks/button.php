88059d8a5d2768af17dc2370fa3b36805b0890fa
Sonic-san <tevin.abhay@gmail.com>
2021-07-02 16:01:50 +0200

    image qui apparait dans bouton

--- www/site/plugins/button/snippets/blocks/button.php
+++ www/site/plugins/button/snippets/blocks/button.php
@@ -1,6 +1,29 @@
+<?php
+
+/** @var \Kirby\Cms\Block $block */
+$alt     = $block->alt();
+$caption = $block->caption();
+$crop    = $block->crop()->isTrue();
+$link    = $block->link();
+$ratio   = $block->ratio()->or('auto');
+$round   = $block->round()->isTrue();
+$src     = null;
+
+if ($block->location() == 'web') {
+    $src = $block->src();
+} elseif ($image = $block->image()->toFile()) {
+    $alt = $alt ?? $image->alt();
+    $src = $image->url();
+}
+
+?>
+
+<img src="<?= $src ?>" alt="<?= $alt ?>" round="<?= $round ?>">
+<div class="externallink">
 <a href="<?= $block->link()->toUrl() ?>">
   <?= $block->text()->html() ?>
 </a>
 <small>
 <?= $block->small()->html() ?>
 </small>
+</div>
