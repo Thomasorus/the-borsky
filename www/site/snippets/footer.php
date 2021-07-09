ab8865161a082c0ba7f2b2ac68170f549afb05c5
Thomasorus <contact@thomasorus.com>
2021-07-10 01:24:54 +0200

    Added missing changes

--- www/site/snippets/footer.php
+++ www/site/snippets/footer.php
@@ -1,12 +1,16 @@
 <script>
     const menuButton = document.querySelector('.mobile-button')
-    const content = document.querySelector("main")
+    const main = document.querySelector("main")
     const nav = document.querySelector("nav")
     const clickHandler = function () {
         if(nav.hasAttribute("open")) {
             nav.removeAttribute("open")
+            window.setTimeout( function() {
+                main.removeAttribute("hidden")
+            }, 400 );
         } else {
             nav.setAttribute("open", "")
+            main.setAttribute("hidden", "")
             window.setTimeout( function() {
                 document.body.scrollTop = 0;
                 document.documentElement.scrollTop = 0
