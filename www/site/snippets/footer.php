35b1784b96dea48ab02268bb1fd34e5c204a4a7e
Thomasorus <contact@thomasorus.com>
2021-07-09 23:58:27 +0200

    Added js used to toggle mobile menu

--- www/site/snippets/footer.php
+++ www/site/snippets/footer.php
@@ -1,2 +1,19 @@
+<script>
+    const menuButton = document.querySelector('.mobile-button')
+    const content = document.querySelector("main")
+    const nav = document.querySelector("nav")
+    const clickHandler = function () {
+        if(nav.hasAttribute("open")) {
+            nav.removeAttribute("open")
+        } else {
+            nav.setAttribute("open", "")
+            window.setTimeout( function() {
+                document.body.scrollTop = 0;
+                document.documentElement.scrollTop = 0
+            }, 100 );
+        }
+    }
+    menuButton.addEventListener('click', clickHandler)
+</script>
 </body>
 </html>
